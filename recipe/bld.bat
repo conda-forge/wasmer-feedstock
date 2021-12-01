00527b0f99a4bc6f40f8be51fa889cb536d344b3
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-12-01 08:13:33 -0600

    try cargo-bundle-licenses

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -14,16 +14,8 @@ dir %WASMER_BIN%
 md %SCRIPTS% || echo "%SCRIPTS% already exists"
 move %WASMER_BIN% %SCRIPTS% || goto :error
 
-:: install cargo-license and dump licenses
-set CARGO_LICENSE_BIN=%BUILD_PREFIX%\bin\cargo-license.exe
-set CARGO_LICENSES_FILE=%SRC_DIR%\%PKG_NAME%-%PKG_VERSION%-cargo-dependencies.json
-
-cargo install --root "%BUILD_PREFIX%" cargo-license
-dir %CARGO_LICENSE_BIN% || goto :error
-%CARGO_LICENSE_BIN% --json > %CARGO_LICENSES_FILE%
-dir %CARGO_LICENSES_FILE% || goto :error
-:: TODO: remove this?
-type %CARGO_LICENSES_FILE% || goto :error
+:: dump licenses
+cargo-bundle-licenses --format yaml --output %SRC_DIR%\THIRDPARTY.yml
 
 :: remove extra build files
 del /F /Q "%PREFIX%\.crates2.json"
--- recipe/build.sh
+++ recipe/build.sh
@@ -15,14 +15,7 @@ cd lib/cli
 # build statically linked binary with Rust
 cargo install --locked --root "$PREFIX" --path . --features "cranelift llvm singlepass"
 
-# install cargo-license and dump licenses
-export CARGO_LICENSES_FILE=$SRC_DIR/$PKG_NAME-$PKG_VERSION-cargo-dependencies.json
-
-cargo install --root "$BUILD_PREFIX" cargo-license
-cargo-license --json > $CARGO_LICENSES_FILE
-ls -lathr $CARGO_LICENSES_FILE
-# TODO: remove this?
-cat $CARGO_LICENSES_FILE
+cargo-bundle-licenses --format yaml --output ${SRC_DIR}/THIRDPARTY.yml
 
 # remove extra build files
 rm -f "${PREFIX}/.crates2.json"
