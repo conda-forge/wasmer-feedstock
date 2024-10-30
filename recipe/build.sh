1351e9947bb22f86671dce44c694910181ae102b
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-10-30 15:50:19 -0500

    bump LLVM_SYS_*_PREFIX

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 export RUST_BACKTRACE=1
-export LLVM_SYS_110_PREFIX=$PREFIX
+export LLVM_SYS_180_PREFIX=$PREFIX
 
 export FEATURES="cranelift singlepass"
 
