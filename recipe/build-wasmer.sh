55b648a8f403e78ab197a6283662b349516afcce
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-04-25 08:39:35 -0500

    linting

--- recipe/build-wasmer.sh
+++ recipe/build-wasmer.sh
@@ -2,19 +2,21 @@
 # NOTE: mostly derived from https://github.com/conda-forge/py-spy-feedstock/blob/master/recipe/build.sh
 set -o xtrace -o nounset -o pipefail -o errexit
 
+_UNAME=$(uname)
+
 export RUST_BACKTRACE=1
 
 export FEATURES="cranelift singlepass"
 
-if [[ "$(uname)" == "Darwin" ]]; then
+if [[ "${_UNAME}" == "Darwin" ]]; then
   # Fix headerpad-max-install-error
   # install_name_tool: changing install names or rpaths can't be redone for
   #  (for architecture x86_64) because larger updated load commands do not fit (the program must be relinked, and you may need to use -headerpad or -headerpad_max_install_names)
   export RUSTFLAGS="-C link-args=-Wl,-rpath,${PREFIX}/lib,-headerpad_max_install_names"
 else
   export LLVM_ENABLE=1
-  export LLVM_SYS_180_PREFIX=$PREFIX
-  export FEATURES="$FEATURES llvm"
+  export LLVM_SYS_180_PREFIX="${PREFIX}"
+  export FEATURES="${FEATURES} llvm"
   export RUSTFLAGS="-C link-arg=-Wl,-rpath-link,${PREFIX}/lib -L${PREFIX}/lib"
 fi
 
@@ -23,15 +25,12 @@ cd lib/cli
 # build statically linked binary with Rust
 cargo install \
   --locked \
-  --root "$PREFIX" \
-  --features "$FEATURES" \
-  --jobs "$CPU_COUNT" \
+  --no-track \
+  --root "${PREFIX}" \
+  --features "${FEATURES}" \
+  --jobs "${CPU_COUNT}" \
   --path .
 
 cargo-bundle-licenses \
   --format yaml \
-  --output ${SRC_DIR}/THIRDPARTY.yml
-
-# remove extra build files
-rm -f "${PREFIX}/.crates2.json"
-rm -f "${PREFIX}/.crates.toml"
+  --output "${SRC_DIR}/THIRDPARTY.yml"
